592d05fcae38f3499529763fed8c497425fe9616
Andrew Stacey <loopspace@mathforge.org>
2019-12-25 21:13:09 +0000

    1. Button code simplified to take a list of keys rather than separate functions for each button 2. Separated the 'down' and 'up' code for buttons to enable long-press 3. Simplified code for stylus buttons 4. Converted configuration to an 'ini' so that it is safer when run under sudo

--- pinspiroy.py
+++ pinspiroy.py
@@ -6,34 +6,80 @@ import usb.core
 import usb.util
 import gtk
 
-import default as g
-try:
-	file_name = sys.argv[1].replace(".py","")
-	file_handler, file_path, module_description = imp.find_module(file_name,[os.getcwd()])
-	g = imp.load_module(file_name, file_handler, file_path, module_description)
-	print(sys.argv[1] + " configuration loaded")
-except:
-	print("default configuration loaded")
+import ConfigParser
+
+config = ConfigParser.RawConfigParser()
+config.read([os.path.dirname(os.path.realpath(sys.argv[0])) + '/config.ini',os.path.expanduser('~/.pinspiroy.ini'),'./.pinspiroy.ini'])
+
+g = {
+    'LEFT_HANDED': False,
+    'PRESSURE_CURVE': False,
+    'FULL_PRESSURE': 1.0,
+    'MONITOR_X': 0,
+    'MONITOR_Y': 0,
+    'MONITOR_W': 1920,
+    'MONITOR_H': 1080,
+    'BUTTONS': [],
+    'RBUTTONS': []
+}
+
+for b in range(11):
+    g['BUTTONS'].append([])
+    g['RBUTTONS'].append([])
+
+if config.has_section('Settings'):
+
+    if config.has_option('Settings','LEFT_HANDED'):
+        g['LEFT_HANDED'] = config.getboolean('Settings','LEFT_HANDED')
+
+    if config.has_option('Settings','PRESSURE_CURVE'):
+        g['PRESSURE_CURVE'] = config.get('Settings','PRESSURE_CURVE')
+
+    if config.has_option('Settings','FULL_PRESSURE'):
+        g['FULL_PRESSURE'] = config.getfloat('Settings','FULL_PRESSURE')
+
+    if config.has_option('Settings','MONITOR_X'):
+        g['MONITOR_X'] = config.getint('Settings','MONITOR_X')
+
+    if config.has_option('Settings','MONITOR_Y'):
+        g['MONITOR_Y'] = config.getint('Settings','MONITOR_Y')
+
+    if config.has_option('Settings','MONITOR_W'):
+        g['MONITOR_W'] = config.getint('Settings','MONITOR_W')
+
+    if config.has_option('Settings','MONITOR_H'):
+        g['MONITOR_H'] = config.getint('Settings','MONITOR_H')
+
+if config.has_section('Buttons'):
+
+    for b in range(11):
+        if config.has_option('Buttons', str(b+1)):
+            g['BUTTONS'][b] = config.get('Buttons', str(b+1)).upper().split('+')
 
 
+for b in range(11):
+        for k in range(len(g['BUTTONS'][b])):
+                if g['BUTTONS'][b][k] == 'CTRL' or g['BUTTONS'][b][k] == 'ALT' or g['BUTTONS'][b][k] == 'SHIFT':
+                        g['BUTTONS'][b][k] = 'LEFT' + g['BUTTONS'][b][k]
+                
+        rb = g['BUTTONS'][b][:]
+        rb.reverse()
+        g['RBUTTONS'][b] = rb
+        
 #tablet config values
-PEN_MAX_X = 50800
-PEN_MAX_Y = 31750 
-PEN_MAX_Z = 2048 	#pressure
+PEN_MAX_X = 45720
+PEN_MAX_Y = 28580 
+PEN_MAX_Z = 8191 	#pressure
 
 # user specs
 MAX_W = gtk.gdk.screen_width()
 MAX_H = gtk.gdk.screen_height()
 
-x_scale  = g.MONITOR_W/float(MAX_W)
-x_offset = PEN_MAX_X*g.MONITOR_X/float(MAX_W)
-y_scale  = g.MONITOR_H/float(MAX_H)
-y_offset = PEN_MAX_Y*g.MONITOR_Y/float(MAX_H)
+x_scale  = g['MONITOR_W']/float(MAX_W)
+x_offset = PEN_MAX_X*g['MONITOR_X']/float(MAX_W)
+y_scale  = g['MONITOR_H']/float(MAX_H)
+y_offset = PEN_MAX_Y*g['MONITOR_Y']/float(MAX_H)
 
-STYLUS_COUNT = 0
-STYLUS_DELAY = 12
-
-msc = 1
 #specify capabilities for a virtual device
 #one for each device:
 #pen/pad, and buttons
@@ -44,13 +90,15 @@ cap_pen = {
 	ecodes.EV_ABS: [
 		(ecodes.ABS_X, AbsInfo(0,0,PEN_MAX_X,0,0,5080)), #value, min, max, fuzz, flat, resolution
 		(ecodes.ABS_Y, AbsInfo(0,0,PEN_MAX_Y,0,0,5080)),
-		(ecodes.ABS_PRESSURE, AbsInfo(0,0,PEN_MAX_Z,0,0,0)),],
+		(ecodes.ABS_PRESSURE, AbsInfo(0,0,PEN_MAX_Z,0,0,0)),
+		(ecodes.ABS_RX, AbsInfo(0,0,256,0,0,60)), #value, min, max, fuzz, flat, resolution
+		(ecodes.ABS_RY, AbsInfo(0,0,256,0,0,60)),
+        ],
 	ecodes.EV_MSC: [ecodes.MSC_SCAN], #not sure why, but it appears to be needed
 	}
 
 #buttons must be defined in the same sequential order as in the Linux specs
 #https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
-
 
 cap_btn = {
 	ecodes.EV_KEY: [ecodes.KEY_ESC,
@@ -69,36 +117,65 @@ vbtn	= UInput(cap_btn, 	name="pinspiroy-button", 	version=0x5)
 
 time.sleep(0.1) # needed due to some xserver feature 
 
+cbtn = -1 # hold current button
+
+# reverse button order for LH setting
+btn_switch_LH = [11,10,9,4,5,6,7,8,3,2,1]
+
 # input specific functions
 def id_btn(data):
         btn = data[5]*256 + data[4]
-	if g.LEFT_HANDED:
-		btn_switch_LH[btn](vbtn)
-	else:
-		btn_switch[btn](vbtn)
+        n = -1
+        while btn != 0:
+                btn = btn >> 1
+                n += 1
+        if n == -1:
+                undo_btn()
+                return
+	if g['LEFT_HANDED']:
+                n = btn_switch_LH[n]
+        do_btn(n)
 
+def do_btn(btn):
+        global cbtn
+        cbtn = btn
+        for k in g['BUTTONS'][btn]:
+                vbtn.write(ecodes.EV_KEY,getattr(ecodes,'KEY_' + k),1)
+        vbtn.syn()
+
+def undo_btn():
+        if cbtn == -1:
+                return
+        for k in g['RBUTTONS'][cbtn]:
+                vbtn.write(ecodes.EV_KEY,getattr(ecodes,'KEY_' + k),0)
+        vbtn.syn()
+                
 def pressure_curve(z):
-	z = z/g.FULL_PRESSURE
+	z = z/g['FULL_PRESSURE']
 	if z > PEN_MAX_Z:
 		z = PEN_MAX_Z
-	if g.PRESSURE_CURVE == 'LINEAR':
+	if g['PRESSURE_CURVE'] == 'LINEAR':
 		pass
-	elif g.PRESSURE_CURVE == 'HARD':
+	elif g['PRESSURE_CURVE'] == 'HARD':
 		z = z*z/PEN_MAX_Z
-	elif g.PRESSURE_CURVE == 'SOFT':
+	elif g['PRESSURE_CURVE'] == 'SOFT':
 		z = z*math.sqrt(z)/math.sqrt(PEN_MAX_Z)
-	return math.floor(z)
+	return int(math.floor(z))
 
 #handler for pen input
 def id_pen(data):
+        
 	x = data[3]*256 + data[2]
 	y = data[5]*256 + data[4]
 	z = data[7]*256 + data[6]
-	
-	if g.PRESSURE_CURVE:
+
+        rx = data[10]
+        ry = data[11]
+        
+	if g['PRESSURE_CURVE']:
 		z = pressure_curve(z)
 	#rotate coordinates if left handed
-	if g.LEFT_HANDED:
+	if g['LEFT_HANDED']:
 		x = PEN_MAX_X-x
 		y = PEN_MAX_Y-y
 
@@ -108,78 +185,39 @@ def id_pen(data):
 	vpen.write(ecodes.EV_ABS, ecodes.ABS_X, x)
 	vpen.write(ecodes.EV_ABS, ecodes.ABS_Y, y)
 	vpen.write(ecodes.EV_ABS, ecodes.ABS_PRESSURE, z)
+	vpen.write(ecodes.EV_ABS, ecodes.ABS_RX, rx)
+	vpen.write(ecodes.EV_ABS, ecodes.ABS_RY, ry)
 
-	global STYLUS_COUNT, STYLUS_DELAY
 
-
-	if data[1] == 128: # pen registered, but not touching pad
-		# reset STYLUS2 delay timer
-		STYLUS_COUNT = 0
-		
-		# stylus buttons released
-		vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS, 0)
-		vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS2, 0)
-
-		vpen.write(ecodes.EV_KEY, ecodes.BTN_TOUCH, 0)
-	elif data[1] == 129: # == 129; pen touching pad
+        if data[1] & 1 == 1:
+                # pen is touching pad
 		vpen.write(ecodes.EV_KEY, ecodes.BTN_TOUCH, 1)
-	elif data[1] == 130: # stylus button 1
-		
+        elif data[1] & 1 == 0:
+		vpen.write(ecodes.EV_KEY, ecodes.BTN_TOUCH, 0)
+                
+        if data[1] & 2 == 2:
+                # stylus button 1
 		vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS, 1)
-	elif data[1] == 132: # stylus button 2
-		# touching the pad with STYLUS1 pressed sends a STYLUS2 signal for a brief moment
-		# to prevent accidental STYLUS2 clicks I've added a brief delay on this click
-		STYLUS_COUNT += 1
-		if STYLUS_COUNT > STYLUS_DELAY:
-			vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS2, 1)
+        elif data[1] & 2 == 0:
+		vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS, 0)
+                
+        if data[1] & 4 == 4:
+                # stylus button 2
+		vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS2, 1)
+        elif data[1] & 4 == 0:
+		vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS2, 0)
 
 	vpen.write(ecodes.EV_KEY, ecodes.BTN_TOOL_PEN, 1)
 
 	vpen.syn() #sync all inputs together
 
-# switch to handle input types
-input_switch = {
-	224:id_btn, #buttonpad 
-	129:id_pen, #stylus down
-	128:id_pen, #stylus up
-	130:id_pen, #stylus button 1
-	132:id_pen,	#stylus button 2
-        0:id_pen, #stylus detached
-}
-
-# switch to handle button types
-btn_switch = {
-	   1:g.btn1, #top 3 buttons
-	   2:g.btn2, 
-	   4:g.btn3, 
-	   8:g.btn4, #wheel: top
-	  16:g.btn5, # bottom
-       	  32:g.btn6, # centre
-          64:g.btn7, # left
-         128:g.btn8, # right
-         256:g.btn9, #bottom 3 buttons
-         512:g.btn10,
-        1024:g.btn11,
-
-	0 :g.btn0, #button released
-}
-
-# reverse button order for LH setting
-btn_switch_LH = {
-	   1:g.btn11, #bottom 3 buttons
-	   2:g.btn10, 
-	   4:g.btn9, 
-	   8:g.btn4, #wheel: bottom
-	  16:g.btn5, # top
-       	  32:g.btn6, # centre
-          64:g.btn7, # right
-         128:g.btn8, # left
-         256:g.btn3, #top 3 buttons
-         512:g.btn2,
-        1024:g.btn1,
-
-	0 :g.btn0, #button released
-}
+def clear():
+        undo_btn()
+        vpen.write(ecodes.EV_KEY, ecodes.BTN_TOOL_PEN, 0)
+        vpen.write(ecodes.EV_KEY, ecodes.BTN_TOUCH, 0)
+        vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS, 0)
+        vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS2, 0)
+        vpen.syn()
 
 # get unidentified huion USB device
 # boilerplate USB data reading from pyusb library
@@ -208,7 +246,13 @@ while True:
 	try:
 		# data received as array of [0,255] ints
 		data = dev.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
-		input_switch[data[1]](data)
+                if data[1] & 64 == 64:
+                        id_btn(data)
+                elif data[1] & 128 == 128:
+                        id_pen(data)
+                else:
+                        clear()
+
 	except usb.core.USBError as e:
 		data = None
 		if e.args == ('Operation timed out',):
